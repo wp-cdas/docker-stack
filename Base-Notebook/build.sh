092dc8311df32aa6fe34ce45745df758618dbe21
Bryan <bryan@Bryans-MacBook-Pro.local>
2020-09-19 11:32:05 -0400

    Change build

--- Base-Notebook/build.sh
+++ Base-Notebook/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 docker build -t cdasdsp/base-notebook:tf-base .
 docker push cdasdsp/base-notebook:tf-base
-=======
-docker build -t cdasdsp/base-notebook-tf:latest .
-docker push cdasdsp/base-notebook-tf:latest
->>>>>>> f9e8bc2da4b5def4223b90a47d266874d4a2ae03
